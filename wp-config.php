cd4924196eed526022d366bc2d033708c57f634d
papavlov <43462771+papavlov@users.noreply.github.com>
2020-02-20 16:19:37 +0200

    Test site made multisite; multisite network now available

--- wp-config.php
+++ wp-config.php
@@ -83,6 +83,13 @@ define( 'WP_DEBUG', false );
 
 /* That's all, stop editing! Happy publishing. */
 
+define('MULTISITE', true);
+define('SUBDOMAIN_INSTALL', false);
+define('DOMAIN_CURRENT_SITE', 'localhost');
+define('PATH_CURRENT_SITE', '/diplomna_rabota/');
+define('SITE_ID_CURRENT_SITE', 1);
+define('BLOG_ID_CURRENT_SITE', 1);
+
 /** Absolute path to the WordPress directory. */
 if ( ! defined( 'ABSPATH' ) ) {
 	define( 'ABSPATH', dirname( __FILE__ ) . '/' );
